2c04c9c57d03572c69fc6888cb2552eb6d9847ff
Richard Li <lichard49@gmail.com>
2020-04-03 03:36:55 -0700

    Refactored audio stimuli code into its own file for other demos

---
+++ js/stimuli.js
@@ -0,0 +1,27 @@
+// helper functions for text-to-speech and playing tones
+function speak(text, callback) {
+  var utter = new SpeechSynthesisUtterance(text);
+  utter.onend = function() {
+    setTimeout(callback, 500);
+  };
+  window.speechSynthesis.cancel();
+  window.speechSynthesis.speak(utter);
+}
+
+function playTone(frequency, duration, callback) {
+  var audio_context = new (window.AudioContext ||
+    window.webkitAudioContext)();
+  var oscillator = audio_context.createOscillator();
+
+  var tempo = 100;
+  duration = 256/(duration*tempo);
+
+  oscillator.type = 'sine';
+  oscillator.frequency.value = frequency; // value in hertz
+  oscillator.connect(audio_context.destination);
+  oscillator.onended = function() {
+    setTimeout(callback, 500);
+  };
+  oscillator.start(0);
+  oscillator.stop(audio_context.currentTime + duration);
+}
